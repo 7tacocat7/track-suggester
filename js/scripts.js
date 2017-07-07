c382b45d115e20ad9ddb8404536f8f4c5f3ff4f9
Carson Canales <cc020889@icloud.com>
2017-07-07 16:56:06 -0700

    do nothing. note date does not display

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,29 @@ $(document).ready(function(){
   $("#thisForm").submit(function(event){
     var myName = $("input#name").val();
     var myEmail = $("input#email").val();
-    var
+    var myQuestion1 = $("#question1").val();
+    var myQuestion2 = $("#question2").val();
+    var myQuestion3 = $("#question3").val();
+    var myQuestion4 = $("#question4").val();
+    var myQuestion5 = $("#question5").val();
+    var mySkill = $("input:radio[name=skill]:checked").val();
+    var myDate = $("#date").val();
+    var myColor = $("#color").val();
 
+    $("#aname").text(myName);
+    $("#aemail").text(myEmail);
+    $("#aquestiont1").text(myQuestion1);
+    $("#aquestiont2").text(myQuestion2);
+    $("#aquestiont3").text(myQuestion3);
+    $("#aquestiont4").text(myQuestion4);
+    $("#aquestiont5").text(myQuestion5);
+    $("#askill").text(mySkill);
+    $("#adate").text(myDate);
+    $("#acolor").text(myColor);
 
+    $(".form2").show();
+    $(".form1").hide();
+
+    event.preventDefault();
   });
 });
